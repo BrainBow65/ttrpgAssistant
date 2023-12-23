9f77a3064d456af77b41641011b5eecefcf9e1d7
BrainBow65 <105944747+BrainBow65@users.noreply.github.com>
2023-12-23 16:33:22 +0100

    12-23-0223

--- ttrpgAssistant/main.py
+++ ttrpgAssistant/main.py
@@ -1,4 +1,4 @@
-import tabs as T
+import TabClasses as T
 
 class ApplicationSettings:
     def __init__(self):
@@ -7,6 +7,57 @@ class ApplicationSettings:
         self.tab2_state = {}
         self.tab3_state = {}
         # ... add state attributes for each tab as needed ...
+
+class EncounterTab(Tab):
+    def __init__(self):
+        super().__init__()
+
+        nested_tab_widget = QTabWidget()
+
+        diplomatic_tab = T.DiplomaticEncounter()
+        nested_tab_widget.addTab(diplomatic_tab, "Diplomatic Function")
+
+        convince_tab = T.ConvinceEncounter()
+        nested_tab_widget.addTab(convince_tab, "Convince Encounter")
+
+        infiltration_tab = T.InfiltrationEncounter()
+        nested_tab_widget.addTab(infiltration_tab, "Infiltration Encounter")
+
+        # interrogation_tab = InterrogationTab()
+        # nested_tab_widget.addTab(interrogation_tab, "Interrogation Encounter")
+
+        # randd_tab = RandDTab()
+        # nested_tab_widget.addTab(randd_tab, "Research and Development")
+
+        T.layout.addWidget(nested_tab_widget)
+        self.setLayout(T.layout)
+        
+        self.save_button = QPushButton("Save")
+        self.save_button.clicked.connect(self.save_state)
+        T.layout.addWidget(self.save_button)
+
+        self.setLayout(T.layout)
+
+        # Hypothetical state variables
+        self.some_state_variable = ""
+        self.another_state_variable = 0
+
+    def set_state(self, state):
+        # Set the state of this tab based on the provided 'state' dictionary
+        self.some_state_variable = state.get("some_state_variable", "")
+        self.another_state_variable = state.get("another_state_variable", 0)
+
+    def get_state(self):
+        # Get the current state of this tab as a dictionary
+        return {
+            "some_state_variable": self.some_state_variable,
+            "another_state_variable": self.another_state_variable,
+        }
+    def save_state(self):
+        # When the Save button is clicked, save the state
+        state = self.get_state()
+        # You can save this state to a file or use it as needed
+        print("State saved:", state)  
 
 class MainWindow(T.QMainWindow):
     def __init__(self):
