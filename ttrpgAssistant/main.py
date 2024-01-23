a53026d55f7a2c754feffa116f54f9492e32ed79
BrainBow65 <105944747+BrainBow65@users.noreply.github.com>
2024-01-23 12:27:20 +0100

    load function still not working totally well, can't load individual instances of npc classes

--- ttrpgAssistant/main.py
+++ ttrpgAssistant/main.py
@@ -92,7 +92,7 @@ def main():
     registry = S.Registry()
     window = MainWindow()
     window.show()
-    app.aboutToQuit.connect(lambda:registry.save_instances())
+    app.aboutToQuit.connect(registry.save_instances())
     T.sys.exit(app.exec())
 
 #something fucky is going on when using static methods to call registry methods. instead need to create instance of registry and use it to initialize and modify the instances dictionary
